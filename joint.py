004dcc6ca290715e0676e67b4caf97472a39cac7
Ayaana96 <34252506+Ayaana96@users.noreply.github.com>
2017-12-04 14:42:24 -0800

    Update and rename denavit_hartengberg_classes.py to joint.py

---
+++ joint.py
@@ -0,0 +1,20 @@
+
+'''This class establishes a joint, whether prismatic or revolute'''
+class Joint(object):
+	
+	def __init__(self, type):
+		self.type = type
+		
+	
+	# New coordinate system is rotated on the x axis by alpha after we've established the z axis
+	
+		
+	
+		
+
+	
+
+
+
+
+
