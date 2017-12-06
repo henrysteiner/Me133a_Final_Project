bec77474acd5b78f61885109dd91d4afa4ab468d
Maia Stiber <mwstiber@gmail.com>
2017-12-05 20:43:53 -0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -56,7 +56,9 @@ class Show_GUI():
 
 		def update_link():
 			while self.currentJoint.ID <= self.numJoints:
+				print("Yes")
 				newJoint = self.joints[self.currentJoint.ID + 1]
+				print("No")
 				self.currentJoint.defineNew(newJoint)
 				self.drawJoints()
 
